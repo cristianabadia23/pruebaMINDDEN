73fed8c9f01803865855dec8b70384ba05694332
DESKTOP-CRB5D67\lisbeth <cristianrabadia@gmail.com>
2023-12-31 12:29:45 +0100

    implemets call to api and recyclerview

--- randomuserlist/app/src/main/java/com/example/randomuserlist/MainActivity.kt
+++ randomuserlist/app/src/main/java/com/example/randomuserlist/MainActivity.kt
@@ -2,10 +2,71 @@ package com.example.randomuserlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.randomuserlist.inferfaces.ApiServiceable
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.Call
 
 class MainActivity : AppCompatActivity() {
+
+    private val apiService = Retrofit.Builder()
+        .baseUrl("https://randomuser.me/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(ApiServiceable::class.java)
+
+    private val userAdapter = UserAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        lifecycleScope.launch {
+            val response = apiService.getUsers()
+            userAdapter.addUsers(response.results)
+        }
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView: RecyclerView = findViewById(R.id.userRecycler)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = userAdapter
+
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+                val lastVisibleItemPosition = layoutManager.findLastVisibleItemPosition()
+                val totalItemCount = layoutManager.itemCount
+
+                if (lastVisibleItemPosition + 1 == totalItemCount) {
+                    lifecycleScope.launch {
+                        loadMoreUsers()
+                    }
+                }
+            }
+        })
+        lifecycleScope.launch {
+            loadMoreUsers()
+        }
+    }
+
+    private suspend fun loadMoreUsers() {
+        try {
+            val response = apiService.getUserbyResults(10)
+            userAdapter.addUsers(response.results)
+            for (user in response.results){
+                println("user = ${user.name.first}")
+            }
+        } catch (e: Exception) {
+            Toast.makeText(this@MainActivity,"Error al cargar user", Toast.LENGTH_LONG).show()
+        }
     }
 }
---
+++ randomuserlist/app/src/main/java/com/example/randomuserlist/UserAdapter.kt
@@ -0,0 +1,51 @@
+package com.example.randomuserlist
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
+import com.example.randomuserlist.models.User
+
+class UserAdapter : RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+
+    private val users = mutableListOf<User>()
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun addUsers(newUsers: List<User>) {
+        users.addAll(newUsers)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.item_user, parent, false)
+        return UserViewHolder(view)
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        val user = users[position]
+
+        holder.nameTextView.text = "${user.name.first} ${user.name.last}"
+        holder.emailTextView.text = user.email
+
+        Glide.with(holder.itemView.context)
+            .load(user.picture.thumbnail)
+            .transform(CircleCrop())
+            .into(holder.userImageView)
+
+    }
+
+    override fun getItemCount() = users.size
+
+    class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val nameTextView: TextView = itemView.findViewById(R.id.nameTextView)
+        val emailTextView: TextView = itemView.findViewById(R.id.emailTextView)
+        val userImageView: ImageView = itemView.findViewById(R.id.userImageView)
+    }
+}
